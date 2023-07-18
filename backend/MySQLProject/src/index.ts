8606610a4f94271327d41c5b78870fb860c13b25
Ettore Farinelli <ettore.farinelli@studio.unibo.it>
2023-07-18 22:20:57 +0200

    boh errori a manetta

--- backend/MySQLProject/src/index.ts
+++ backend/MySQLProject/src/index.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { Lottatore } from './entity/lottatore';
+import { amministratore } from './entity/amministratore';
+import { categoria } from './entity/categoria';
+import { disciplina } from './entity/disciplina';
 
 const AppDataSource = new DataSource({
     type: "mysql",
@@ -29,15 +32,46 @@ const AppDataSource = new DataSource({
 }).catch(error => console.log(error));*/
 
 AppDataSource.initialize().then(async connection => {
-    let lottatore = new Lottatore();
-    lottatore.nome = "Oracle Magazine";
-    lottatore.cognome = "Oracle Publishing";
-    lottatore.codiceFiscale = "March-April 2005";
-    lottatore.team = "sakfnas";
-    lottatore.arteMarziale = "caio";
-    lottatore.dataNascita = new Date(1999, 22, 1);
-    lottatore.peso = 30;
+    let Amministratore = new amministratore();
+    let Categoria1 = new categoria();
+    let Categoria2 = new categoria();
+    let Categoria3 = new categoria();
+    let Categoria4 = new categoria();
+    let Disciplina1 = new disciplina();
+    let Disciplina2 = new disciplina();
+    let Disciplina3 = new disciplina();
 
-    await connection.manager.save(lottatore);
+    Amministratore.username = "TheDarkRuler";
+    await connection.manager.save(Amministratore);
+
+    /*Categoria1.nome = "Peso Piuma"
+    Categoria1.pesoMassimo = 65
+    await connection.manager.save(Categoria1);
+
+    Categoria2.nome = "Welterweight"
+    Categoria2.pesoMinimo = 66
+    Categoria2.pesoMassimo = 77
+    await connection.manager.save(Categoria2);
+
+    Categoria3.nome = "Peso Medio"
+    Categoria3.pesoMinimo = 78
+    Categoria3.pesoMassimo = 84
+    await connection.manager.save(Categoria3);
+
+    Categoria4.nome = "Pesi Massimi"
+    Categoria4.pesoMinimo = 85
+    await connection.manager.save(Categoria4);
+
+    Disciplina1.nome = "BJJ"
+    await connection.manager.save(Categoria1);
+
+    Disciplina2.nome = "MMA"
+    await connection.manager.save(Categoria2);
+
+    Disciplina3.nome = "MuayThai"
+    await connection.manager.save(Categoria3);*/
 
 }).catch(error => console.log(error));
+
+/*const ciao = AppDataSource.query(`SELECT * FROM lottatore`);
+console.log(ciao);*/
