69ce99b74e9411102288363dd0aaa51cc8ddacf3
Ettore Farinelli <ettore.farinelli@studio.unibo.it>
2023-07-21 23:05:16 +0200

    progress in the app

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import background from './img/mma-background.jpg';
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Statistics from './Statistics/Statistics';
+import Operations from './Operations/Operations';
+
+function OperationButton() {
+
+  let navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    let path = `Operations`; 
+    navigate(path, {replace: true});
+  }
+  
+  return (
+    <div className='MyButton' onClick={routeChange}>
+      <span> 
+        Operazioni Amministratore  
+      </span>
+    </div>
+  );
+}
+
+function StatisticsButton() {
+
+  let navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    let path = `Statistics`; 
+    navigate(path, {replace: true});
+  }
+
+  return (
+    <div className='MyButton' onClick={routeChange}>
+      <span> 
+        Statistiche      
+      </span>
+    </div>
+  );
+}
 
 function App() {
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="Statistics" element={<Statistics />} />
+        <Route path="Operations" element={<Operations />} />
+      </Routes>
+      <div style={{backgroundImage: `url(${background})`, 
+      backgroundSize: 'cover',
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      height: '100vh',
+      width: '100vw',
+      }}>
+        <OperationButton />
+        <StatisticsButton />
+      </div>
     </div>
   );
 }
---
+++ react_app/src/Operations/Operations.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Operations.css'
+
+export default function Operations() {
+
+    return(
+        <div className='Operations'>
+            <button>
+                ciao
+            </button>
+        </div>
+    );
+}
